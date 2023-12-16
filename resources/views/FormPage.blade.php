b51ba54e2ad3d6ac611994381aa134fb7b5e3f83
Italo0605 <italosatiro2@gmail.com>
2023-12-16 12:18:54 -0300

    melhorando as paginas form e mail

--- resources/views/FormPage.blade.php
+++ resources/views/FormPage.blade.php
@@ -5,14 +5,12 @@
 
         <div class="container flex justify-center items-center mx-auto pt-4">
             <img src="/images/girlForms.svg" alt="" class="self-end">
-            <div class=" flex flex-col justify-center items-center">
-                <div class="relative flex flex-col text-center">
-                    <div class="p-40 text-white rounded-full bg-[#7DA5D2] w-fit"></div>
-                    <p class="text-white relative bottom-[12rem] right-1.5 text-3xl "">JÁ DECIDI QUE<br>VOU GRAVAR</p>
+            <div class="flex flex-col self-start items-center">
+                <div  class="group cursor-pointer w-80 h-80 bg-[#7DA5D2] rounded-full flex justify-center items-center relative right-[80px] border-8 border-[#7DA5D2] hover:bg-white hover:border-8 hover:border-[#7DA5D2] transition-colors duration-500">
+                    <p class="text-white text-3xl transition-colors group-hover:text-black transition-colors duration-500">JÁ DECIDI QUE<br>VOU GRAVAR</p>
                 </div>
-                <div class="relative flex flex-col text-center bottom-[12rem]  left-[6rem]">
-                    <div class="p-28 text-white rounded-full bg-white  w-fit relative"></div>
-                    <p class=" relative text-2xl bottom-[8rem]">E AGORA?</p>
+                <div class="group cursor-pointer flex justify-center items-center w-48 h-48 bg-white border-8 border-white rounded-full relative -right-[50px] -top-[90px] hover:bg-[#00296B] hover:border-8 hover:border-[##00266B] transition-colors duration-500">
+                    <p class="text-[#00296B] text-2xl group-hover:text-white transition-colors duration-500">E AGORA?</p>
                 </div>
             </div>
         </div>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300&family=Didact+Gothic&display=swap" rel="stylesheet">
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              fontFamily: {
+                "didact" : ['Didact Gothic', 'sans-serif'],
+                "dm-sans": ['DM Sans', 'sans-serif']
+              }
+            }
+          }
+        }
+      </script>
+    <title>Document</title>
+</head>
