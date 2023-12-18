0e838b180e0902c12cb278da89a1068861943dca
Italo0605 <italosatiro2@gmail.com>
2023-12-17 21:04:51 -0300

    add copyright collum

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,21 +1,21 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script>
-        tailwind.config = {
-          theme: {
-            extend: {
-              fontFamily: {
-                "didact" : ['Didact Gothic', 'sans-serif'],
-                "dm-sans": ['DM Sans', 'sans-serif']
-              }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>    
+  <script>
+    tailwind.config = {
+        theme: {
+          extend: {
+            fontFamily: {
+              "didact" : ['Didact Gothic', 'sans-serif'],
+              "dm-sans": ['DM Sans', 'sans-serif']
             }
           }
         }
-      </script>
-    <title>Document</title>
+      }
+    </script>
+  <title>Document</title>
 </head>
